4f7747ad8b8806d7b8b3af0d29cbd270c873c232
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-14 10:05:15 -0500

    Write code to calculate area

--- src/App.java
+++ src/App.java
@@ -6,4 +6,20 @@
  */
 
 public class App {
+    public static void main (String[] args) {
+        System.out.println(area(5.0));         // 78.53975
+        System.out.println(area(0.0));         // 0.0
+        System.out.println(area(-1));          // -1.0
+        System.out.println(area(5.0, 4.0));     // 20.0
+        System.out.println(area(-1.0, 4.0));    // -1.0
+        System.out.println(area(4.0, -1.0));    // -1.0
+    }
+
+    public static double area(double radius) {
+        return (radius >= 0) ? radius * radius * Math.PI : -1;
+    }
+
+    public static double area(double x, double y) {
+        return ((x >= 0) && (y >= 0)) ? x * y : -1;
+    }
 }
